7e95d68b5b08562c40d7dfc76eed6928d7993802
Raviraja09 <rajavysyaraju09@gmail.com>
2022-12-09 00:01:34 +0530

    start

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+const header=(props)=>
+{
+ return <Fragment>
+    <header>
+        <h1>Reactmeals</h1>
+        <button>Cart</button>
+    </header>
+    <div></div>
+ </Fragment>
+}
+export default header
